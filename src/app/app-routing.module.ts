7a5c17cca45ba66994ac0a5efa2fdba1095672c8
Ankit Rai <ankitrai800@gmail.com>
2022-07-03 02:44:21 +0530

    initial routing for dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
